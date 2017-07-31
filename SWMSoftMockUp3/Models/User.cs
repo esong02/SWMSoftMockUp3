f8ae16eb607c7655beaf1ec5746423b6824a2d05
Eric Song <esong02@mail.uoguelph.ca>
2017-07-31 15:52:48 -0400

    Created LoginPage, with Username and Password functionality. Still need to implement new user and forgot password

---
+++ SWMSoftMockUp3/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SWMSoftMockUp3.ViewModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserLogon { get; set; }
+        public string UserName { get; set; }
+        public string UserInitials { get; set; }
+        public bool IsAdministrator { get; set; }
+        public bool IsReadOnly { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
